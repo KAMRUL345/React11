9ab8dbc6f421fb4b68b3f0ea797da8e3015497f0
KAMRUL345 <br7100@gmail.com>
2025-07-08 09:13:34 +0600

    component added

---
+++ src/components/FullName.jsx
@@ -0,0 +1,6 @@
+// FullName.js
+function FullName(props) {
+  return <h1>Hello, I am {props.name1} {props.name2}!</h1>;
+}
+
+export default FullName;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Hello from './components/hello.jsx'
+import FullName from './components/FullName.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,5 +13,8 @@ createRoot(document.getElementById('root')).render(
     <Hello name="Kamrul" />
     <Hello name="Bangladesh" />
     <Hello name="Agrani Bank PLC" />
+    <FullName name1="Kamrul" name2="Hasan" />
+    <FullName name1="Masrura" name2="Borsha" />
+
   </StrictMode>,
 )
