0ed229891b90b055d1a93d12c9d905500478de22
KAMRUL345 <br7100@gmail.com>
2025-07-16 08:42:17 +0600

    pages added

---
+++ src/components/footer/Footer1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer1(props) {
+    return (
+        <div>
+        <h1>footer1</h1>
+            
+        </div>
+    );
+}
+
+export default Footer1;
---
+++ src/components/header/Header1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header1(props) {
+    return (
+        <div>
+            <h1>Header1</h1>
+            
+        </div>
+    );
+}
+
+export default Header1;
